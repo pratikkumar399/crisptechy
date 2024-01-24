75fc955f29b301716b0531f390b06338f95dbc22
raipratik <pratikkumar399@gmail.com>
2024-01-24 12:21:08 +0530

    Deploy website - based on e20eedec0424422b2f470c6fa7febf6102776fab

---
+++ assets/js/814f3328.d587bc94.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkcrisptechy=self.webpackChunkcrisptechy||[]).push([[2535],{5641:e=>{e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"intro","permalink":"/crisptechy/blog/intro"},{"title":"ReactEnvironment","permalink":"/crisptechy/blog/ReactEnvironment"}]}')}}]);
---
+++ assets/js/ab3a9458.eccbd7f3.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkcrisptechy=self.webpackChunkcrisptechy||[]).push([[8108],{3820:e=>{e.exports=JSON.parse('{"permalink":"/crisptechy/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":2,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
